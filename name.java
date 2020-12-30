2ae09e7c15d52e226de90f455ef16aaba04f58dd
FrankF13 <47196664+FrankF13@users.noreply.github.com>
2020-12-30 15:57:27 -0500

    Add files via upload

---
+++ name.java
@@ -0,0 +1,21 @@
+
+package com.company;
+
+public class name {
+
+    public static void main(String[] args) {
+        int a = 0;
+        while(a<=1001) {
+            if (a % 3 == 0) {
+                System.out.println(a);
+            } else if (a % 5 == 0)
+                System.out.println(a);
+            else if (a % 15 == 0)
+            System.out.println(a);{
+                a += 1;
+            }
+            {
+            }
+        }
+    }
+}
